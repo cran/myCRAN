2356a06ff1e24a0b2129ea5846a781a7e71ccf18
Barry Zeeberg <barryz2013@gmail.com>
2024-02-15 16:10:02 +0000

    version 1.1

--- R/myCRAN.R
+++ R/myCRAN.R
@@ -1,28 +1,45 @@
 #' myCRAN
 #'
+#' @import cranlogs
+#' @import grDevices
+#' @import graphics
+#' @import pkgsearch
+#'
+#'
 #' @description wrapper for cran_downloads() to plot counts and cumulative counts
 #'	of downloads of CRAN packages
 #'
 #' @param packages character vector of names of packages
+#' @param author character string of name of target author
 #' @param from date parameter for cran_downloads()
 #' @param when date parameter for cran_downloads()
 #' @param plotNew Boolean if TRUE start a new plot window
 #' @param plotWidth integer width parameter for dev.new()
 #'
+#' @details Use either packages or author as the input parameter.
+#' When author is given, the function advanced_search() queries CRAN
+#' to return a list of the names of all packages by that author.
+#'
 #' @examples
-#' 	packages<-c("timeLineGraphics","textBoxPlacement","SherlockHolmes")
+#' \dontrun{
+#' 	packages<-c("timeLineGraphics","textBoxPlacement","SherlockHolmes","myCRAN")
+#' 	author = "Zeeberg"
 #' 	from<-"2023-01-01"
-#' 	myCRAN(packages,from=from,plotNew=TRUE)
-#' 	myCRAN(packages,when="last-week",plotNew=FALSE)
+#' 	myCRAN(author=author,from=from,plotNew=TRUE)
+#' 	myCRAN(packages=packages,when="last-week",plotNew=FALSE)
+#' 	}
 #'
 #' @return returns no values, but has side effect of generating plots
 #'   of daily and cumulative number of downloads of your packages
 #'
 #' @export
 myCRAN<-
-	function(packages,from,when,plotNew=TRUE,plotWidth=15) {
+	function(packages=NULL,author=NULL,from,when,plotNew=TRUE,plotWidth=12) {
 		l<-list()
 		s<-vector("integer",length(packages))
+		if(!is.null(author))
+		  packages<-pkgsearch::advanced_search(Author = author, size = 100)$package
+
 		for(pack in packages) {
 			l[[pack]]<-cran_downloads(pack,from=from,when=when)
 		}
@@ -34,12 +51,23 @@ myCRAN<-
 
 		ylim<-c(0,max)
 
-		colors<-c("green","blue","red","orange","magenta","black")
+		# next list is the original list that I used until around February 14, 2024
+		# colors<-c("green","blue","red","orange","magenta","black")
+		# next list comes from https://sashamaps.net/docs/resources/20-colors/
+		# I omit white from the original list
+
+		# colors<-c("red","green","blue","orange","magenta","pink","teal",
+  	#	    "brown","beige","maroon","mint","olive","apricot","navy","grey","black","lavender","yellow","cyan","lime","purple")
+
+		colors<-c("red","green","blue","orange","magenta","pink","#469990",
+		          "brown","beige","maroon","#aaffc3","#808000","#ffd8b1","navy","grey","black","lavender","yellow","cyan","#bfef45","purple")
+
 		xlabel<-"date"
 		ylabel<-"counts"
 		title<-"My R Packages Downloads"
+
 		if(plotNew)
-			dev.new(width=plotWidth)
+		  dev.new(width=plotWidth,unit="in")
 		plot(x=l[[packages[1]]][,"date"],y=l[[packages[1]]][,"count"],ylim=ylim,type="l",
 			xlab=xlabel,ylab=ylabel,main=title)
 		i<-1
